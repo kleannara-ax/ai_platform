835ddc2f613862fe43a17f9ed9cfbf286e45f171
kleanna-dx <genspark_dev@genspark.ai>
2026-05-15 10:27:27 +0000

    평균 크기 + 표준편차 하나의 카드로 병합

--- module-ps-insp/src/main/resources/static/ps-insp/js/app.js
+++ module-ps-insp/src/main/resources/static/ps-insp/js/app.js
@@ -1136,8 +1136,7 @@
       + qItem('자동 / 수동', m.autoCount + ' / ' + m.manualCount, 'blue')
       + qItem('크기 균일도', (m.sizeUniformityScore * 100).toFixed(1) + '%', 'green')
       + qItem('분포 균일도', (m.distributionUniformityScore * 100).toFixed(1) + '%', 'green')
-      + qItem('평균 크기 (px)', m.meanSize.toFixed(1), 'blue')
-      + qItem('표준편차 (px)', m.stdSize.toFixed(1), 'blue')
+      + qItem('평균 / 표준편차', m.meanSize.toFixed(1) + ' / ' + m.stdSize.toFixed(1) + ' px', 'blue')
       + qItem('삭제 자동검출', m.removedAutoCount, 'red');
 
     // Bucket bar
